0681a89c9fed7f9cbaf00d05a866b8950ca1dd33
zex <745795664@qq.com>
2014-08-22 13:49:54 +0800

    优化代码次序，解决第一次访问book时页面解析错误

--- js/book.js
+++ js/book.js
@@ -6,13 +6,14 @@ $(".subject-description").text(subject_description);
 if($(".right").height()<$(window).height()-100){
 $(".right").height($(window).height()-100);
 };
-for(var i = 0;i< bookArray.length-1;i++){
-	$("#section-outter").clone(true).appendTo("ul");
-}
 for(var i = 0;i< bookArray.length;i++){
-	$("ul a li #section-text").eq(i).text(bookArray[i]);
-	$("ul a").eq(i).attr("href",srcArray[i]);
+  if(i < bookArray.length-1){
+	$("#section-outter").clone(true).appendTo("ul");
+  }
+  $("ul div a li #section-text").eq(i).text(bookArray[i]);
+  $("ul #section-outter a").eq(i).attr("href",srcArray[i]);
 }
+
 $(window).resize(function(){
   if($(".right").height()<$(window).height()-100){
     $(".right").height($(window).height()-100);
