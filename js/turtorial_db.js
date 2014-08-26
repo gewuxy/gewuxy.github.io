a7931cfa0beb16388257a5209d5b0a29ad6f7535
zex <745795664@qq.com>
2014-08-26 14:22:32 +0800

    数据层从html页面中分离出来

--- js/turtorial_db.js
+++ js/turtorial_db.js
@@ -1,90 +1,26 @@
-
-$(document).ready(function(){
-$.get("../turtorial_template.html",function(data){
-$(".flash-page").html(data);
-$('a#section-title').attr("href",back_chapter);
-$('.tutorial-title').text(tutorial_title);
-$('.publisher .teacher').text(publisher);
-$('iframe').attr("src",src);
-for(var i = 0;i< turtorialArray.length;i++){
-  if(i < turtorialArray.length-1){
-	$("#tutorial-tab-link").clone(true).appendTo("ul.tutorial-nav-content-list");
-  }
-  $("ul>a>li>span").eq(i).text(turtorialArray[i]);
-  $("ul.tutorial-nav-content-list a").eq(i).attr("href",srcArray[i]);
+﻿var back_chapter,tutorial_title,publisher,src;
+var srcArray = new Array();
+var turtorialArray = new Array();
+var tutorial_nav_next = new Array();
+switch(turId){
+	case MATH_BIXIU1_CHAPTER1_TUR1_1:
+		back_chapter = "bixiu1_chapter1.html";
+		tutorial_title = "认识集合";
+		publisher = "罗老师";
+		src = "https://www.educreations.com/lesson/embed/21889272/";
+		srcArray = ["javascript:void(0);","bixiu1_chapter1_turtorial1-2.html","bixiu1_chapter1_turtorial1-3.html"];
+		turtorialArray = ["认识集合","集合间的关系","集合运算"];
+		tutorial_nav_next = ["函数篇","bixiu1_chapter1_turtorial2-1.html"];
+		break;
+	case MATH_BIXIU1_CHAPTER1_TUR1_2:
+		back_chapter = "bixiu1_chapter1.html";
+		tutorial_title = "集合间的关系";
+		publisher = "罗老师";
+		src = "https://www.educreations.com/lesson/embed/21889444/";
+		srcArray = ["bixiu1_chapter1_turtorial1-1.html","javascript:void(0);","bixiu1_chapter1_turtorial1-3.html"];
+		turtorialArray = ["认识集合","集合间的关系","集合运算"];
+		tutorial_nav_next = ["函数篇","bixiu1_chapter1_turtorial2-1.html"];
+		break;
+	default:
+		break;
 }
-for(var i = 0;i< turtorialArray.length;i++){
-  if(turtorialArray[i] == tutorial_title){
-	$('ul>a>li').eq(i).addClass("active");
-  }
-}
-if(tutorial_nav_next[0] != ""){
-	$('.tutorial-nav-next-label').text(tutorial_nav_next[0]);
-	$('.tutorial-nav-next a').attr("href",tutorial_nav_next[1]);
-}else{
-	$('.tutorial-nav-next').hide();
-}
-//jiathis分享开始
-
-jQuery.ajax({
-      url: "http://v3.jiathis.com/code_mini/jia.js",
-	  async: false,
-      dataType: "script"
-}).done(function() {
-  return;
-});
-
-//jiathis分享结束
-if($(".tutorial-wrap").width()-250>500){
-$(".flash-box").width($(".tutorial-wrap").width()-250);
-}else{
-$(".flash-box").width("500");
-}
-
-$(".hidelist").click(function(){
-  if(!$(".tutorial-nav").is(":hidden")){
-	$(".tutorial-nav").hide();
-	$(".flash-box").width("100%");
-	$(this).text("显示列表");
-  }else{
-    $(".tutorial-nav").show();
-	$(".flash-box").width($(".tutorial-wrap").width()-250);
-	$(this).text("收起列表");
-  }
-});
-$.get("/footer.html",function(data){
-$("#footer").html(data);
-});
-$('.ds-thread').attr("data-title",tutorial_title);
-$('.ds-thread').attr("data-thread-key",tutorial_title);
-$('.ds-thread').attr("data-url",window.location.href);
-});
-$.get("../../../header.html",function(data){
-$("#header").html(data);
-});
-
-$(window).resize(function(){
-if($(".tutorial-wrap").width()-250>500){
-  if(!$(".tutorial-nav").is(":hidden")){
-	$(".flash-box").width($(".tutorial-wrap").width()-250);
-  }else{
-    $(".flash-box").width("100%");
-  }
-}else{
-$(".flash-box").width("500");
-}
-}); 
-});
-var jiathis_config={
-	siteNum:6,
-	sm:"tsina,qzone,weixin,renren,cqq,douban",
-	summary:"我看到了一个很好的视频课，分享给大家【"+tutorial_title+"】"+"——来自格物学院（www.gewuxy.com）",
-	title:" ",
-	boldNum:0,
-	appkey:{
-		"tsina":"weibo",
-		"tqq":"tencent-weibo"
-	},
-	shortUrl:false,
-	hideMore:true
-};
