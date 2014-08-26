f31aa9a01ae6bfd0fd976dd902f50dee03ffb25a
zex <745795664@qq.com>
2014-08-26 15:06:42 +0800

    同上

--- js/turtorial_db.js
+++ js/turtorial_db.js
@@ -66,6 +66,42 @@ switch(turId){
 		turtorialArray = ["认识函数","函数图像","单调性","奇偶性"];
 		tutorial_nav_next = ["",""];
 		break;
+	case MATH_BIXIU1_CHAPTER2_TUR1_1:
+		back_chapter = "bixiu1_chapter2.html";
+		tutorial_title = "指数运算";
+		publisher = "罗老师";
+		src = "https://www.educreations.com/lesson/embed/22042960/";
+		srcArray = ["javascript:void(0);","bixiu1_chapter2_turtorial1-2.html"];
+		turtorialArray = ["指数运算","指数函数"];
+		tutorial_nav_next = ["对数篇","bixiu1_chapter2_turtorial2-1.html"];
+		break;
+	case MATH_BIXIU1_CHAPTER2_TUR1_2:
+		back_chapter = "bixiu1_chapter2.html";
+		tutorial_title = "指数函数";
+		publisher = "罗老师";
+		src = "https://www.educreations.com/lesson/embed/22043390/";
+		srcArray = ["bixiu1_chapter2_turtorial1-1.html","javascript:void(0);"];
+		turtorialArray = ["指数运算","指数函数"];
+		tutorial_nav_next = ["对数篇","bixiu1_chapter2_turtorial2-1.html"];
+		break;
+	case MATH_BIXIU1_CHAPTER2_TUR2_1:
+		back_chapter = "bixiu1_chapter2.html";
+		tutorial_title = "认识对数";
+		publisher = "罗老师";
+		src = "https://www.educreations.com/lesson/embed/22096754/";
+		srcArray = ["javascript:void(0);","bixiu1_chapter2_turtorial2-2.html","bixiu1_chapter2_turtorial2-3.html","bixiu1_chapter2_turtorial2-4.html","bixiu1_chapter2_turtorial2-5.html"];
+		turtorialArray = ["认识对数","对数运算","对数运算2","对数函数","对数函数2"];
+		tutorial_nav_next = ["",""];
+		break;
+	case MATH_BIXIU1_CHAPTER2_TUR2_2:
+		back_chapter = "bixiu1_chapter2.html";
+		tutorial_title = "对数运算";
+		publisher = "罗老师";
+		src = "https://www.educreations.com/lesson/embed/22099703/";
+		srcArray = ["bixiu1_chapter2_turtorial2-1.html","javascript:void(0);","bixiu1_chapter2_turtorial2-3.html","bixiu1_chapter2_turtorial2-4.html","bixiu1_chapter2_turtorial2-5.html"];
+		turtorialArray = ["认识对数","对数运算","对数运算2","对数函数","对数函数2"];
+		tutorial_nav_next = ["",""];
+		break;
 	default:
 		break;
 }
